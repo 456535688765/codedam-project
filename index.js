073beb99bc1b3aa8ac9ae6bb21e593a143684268
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2024-01-20 16:51:54 +0530

    lab5 commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 let apiKey = "Replace this with your api key"
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+let weatherInfo = document.querySelector(".weather-info")
+
+button.addEventListener("click", displayData)
+
+function displayData(){
+let city = input.value
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=YourApikey
+    `).then((response) => response.json()).then((data) => {
+      displayWeather(data);
+    }).catch((error) => {
+        console.error("Error while Fetching Data:", error);
+        alert("Unable to fetch data, Please Try Again");
+      });
+    
+}
